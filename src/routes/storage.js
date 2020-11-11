fac83e2a0e5bed859ab98a36f24a48e4990bb4e3
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-12 00:09:26 +0100

    Added route to get tree structure of local storage

--- src/routes/storage.js
+++ src/routes/storage.js
@@ -4,5 +4,16 @@ const router = express.Router();
 import { sha256 } from '../models/hash';
 import { Storage } from '../models/storage';
 
+router.get('/tree', async (req, res) => {
+    const user = req.user;
+    const id = sha256(user.id);
+    const key = user.key;
+
+    const storage = new Storage(id, key);
+
+    return res.status(200).json({
+        tree: await storage.tree()
+    });
+});
 
 export default router;
