e07d7083b1389a321df1743ec7a88119a1e60717
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-12 00:09:00 +0100

    Added route controller for storage

---
+++ src/routes/storage.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+import { sha256 } from '../models/hash';
+import { Storage } from '../models/storage';
+
+
+export default router;
