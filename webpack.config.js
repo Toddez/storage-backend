5ed28bbef9208a48f5463e4d9b753b88e488d84d
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-11 22:00:13 +0100

    Added prod/dev mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ let nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/server.js'),
+    mode: process.env.PRODUCTION ? 'production' : 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
