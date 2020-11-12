f4c9f0460674ff6c7120d984427cacd589926b30
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-12 23:26:32 +0100

    Accommodate model changes

--- src/routes/storage.js
+++ src/routes/storage.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-import { sha256 } from '../models/hash';
-import { Storage } from '../models/storage';
+import { Storage, NodeType } from '../models/storage';
 
 router.get('/tree', async (req, res) => {
     const user = req.user;
-    const id = sha256(user.id);
+    const id = user.id;
     const key = user.key;
 
     const storage = new Storage(id, key);
 
     return res.status(200).json({
-        tree: await storage.tree()
+        tree: await storage.export(storage.root),
+        types: NodeType
     });
 });
 
