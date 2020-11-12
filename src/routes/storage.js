13d1ad1f980c5f5fc0a2a3eaa65125b38f975451
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-12 23:41:49 +0100

    Add read and write routes

--- src/routes/storage.js
+++ src/routes/storage.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import { Storage, NodeType } from '../models/storage';
 
-router.get('/tree', async (req, res) => {
+router.get('/', async (req, res) => {
     const user = req.user;
     const id = user.id;
     const key = user.key;
@@ -12,6 +12,40 @@ router.get('/tree', async (req, res) => {
 
     return res.status(200).json({
         tree: await storage.export(storage.root),
+        types: NodeType
+    });
+});
+
+router.get('/read/*', async (req, res) => {
+    const user = req.user;
+    const localPath = req.params[0];
+    const id = user.id;
+    const key = user.key;
+
+    const storage = new Storage(id, key);
+
+    let data = await storage.readFile(localPath);
+
+    return res.status(200).json({
+        path: localPath,
+        data: data,
+        types: NodeType
+    });
+});
+
+router.post('/write/*', async (req, res) => {
+    const user = req.user;
+    const localPath = req.params[0];
+    const data = req.body.data;
+    const id = user.id;
+    const key = user.key;
+
+    const storage = new Storage(id, key);
+
+    await storage.writeFile(localPath, data);
+
+    return res.status(200).json({
+        path: localPath,
         types: NodeType
     });
 });
