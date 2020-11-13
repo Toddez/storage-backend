ea42091dd40f4290ccc8315f7ae7fe5936d8d5b6
Teo Carlsson <carlsson.teo@gmail.com>
2020-11-13 01:18:06 +0100

    Added ability to create directory through /write

--- src/routes/storage.js
+++ src/routes/storage.js
@@ -33,16 +33,25 @@ router.get('/read/*', async (req, res) => {
     });
 });
 
-router.post('/write/*', async (req, res) => {
+router.post('/write/*', async (req, res, next) => {
     const user = req.user;
     const localPath = req.params[0];
+    const type =  req.body.type;
     const data = req.body.data;
     const id = user.id;
     const key = user.key;
 
     const storage = new Storage(id, key);
 
-    await storage.writeFile(localPath, data);
+    if (type & NodeType.FILE)
+        await storage.writeFile(localPath, data);
+    else if (type & NodeType.DIR)
+        await storage.createDir(localPath);
+    else {
+        let err = new Error('Invalid type');
+        err.status = 400;
+        return next(err);
+    }
 
     return res.status(200).json({
         path: localPath,
